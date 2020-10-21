d6aa4acda4cb7e07e07b2e7eb783ef03c9809b44
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-10-20 23:38:26 -0400

    added more chai tests🍵

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,54 @@
-it("works", () => {
-  //   console.log("hello test");
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      reject();
+    }, 2000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHtml = "";
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Not Avengers" },
+        { Title: "Another movie" },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
+});
+
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).not.to.include("is-active");
+});
+
+it("after searching, dropdown opens up", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
+  // delete the widget
+});
+
+it("after searching", () => {
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).not.to.include("is-active");
 });
